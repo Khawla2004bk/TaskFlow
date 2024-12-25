1d87d088d54a2894e04c717c96fccf363e180cf4
Khawla Boukniter <boukniter.khawla@gmail.com>
2024-12-25 21:07:26 +0100

    Transformer le fichier de connexion en une class de connexion

--- config/connexion.php
+++ config/connexion.php
@@ -1,20 +1,23 @@
 <?php
-session_start();
 
-$host = 'localhost';
-$dbname = 'taskflow';
-$username = 'root';
-$password = '';
+class Connexion {
+    private $pdo;
+    
 
-try {
-    $pdo = new PDO ("mysql:host=$host; dbname=$dbname; charset=utf8", $username, $password, [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-        PDO::ATTR_EMULATE_PREPARES => false
-    ]);
-}
-catch (PDOException $e) {
-    error_log("Erreur de connexion à la base de données : " . $e->getMessage());
-    error_log("Détails de l'erreur : " . $e->getMessage());
-    die("Erreur de connexion" . $e->getMessage());
+    public function __construct($host, $dbname, $username, $password) {
+        $host = 'loacalhost';
+        $dbname = 'taskflow';
+        $username = 'root';
+        $password = '';
+
+        try {
+            $this->pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password, [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+            ]);
+        } catch (PDOException $e) {
+            echo "Erreur de connexion: " . $e->getMessage();
+            throw new Exception("Connexion Impossible à la base de données");
+        }
+    }
 }
